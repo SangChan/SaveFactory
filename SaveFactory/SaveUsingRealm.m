491783dad03183e87fc0d26cf8cfeeb5cbcf4e93
SangChan Lee <sangchan82@gmail.com>
2015-08-05 12:33:09 +0900

    add realm api

--- SaveFactory/SaveUsingRealm.m
+++ SaveFactory/SaveUsingRealm.m
@@ -7,22 +7,53 @@
 //
 
 #import "SaveUsingRealm.h"
+#import <Realm/Realm.h>
+
+// Data model
+@interface Data : RLMObject
+@property NSString *key;
+@property id value;
+@end
+
+// Implementations
+@implementation Data
+@end // none needed
 
 @implementation SaveUsingRealm
 
 - (void)create {
-    
 }
 
 - (id)readDataUsingKey:(NSString *)key {
-    return nil;
+    NSString *query = [NSString stringWithFormat:@"key = '%@'",key];
+    RLMResults *datas = [Data objectsWhere:query];
+    return [datas firstObject];
 }
 
 - (void)removeDataUsingKey:(NSString *)key {
+    NSString *query = [NSString stringWithFormat:@"key = '%@'",key];
+    RLMResults *datas = [Data objectsWhere:query];
+    
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    [realm beginWriteTransaction];
+    [realm deleteObjects:datas];
+    [realm commitWriteTransaction];
     
 }
 - (void)writeDataUsingKey:(NSString *)key Value:(id)value {
+    // Create object
+    Data *data = [[Data alloc]init];
+    data.key = key;
+    data.value = value;
     
+    // Get the default Realm
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    // You only need to do this once (per thread)
+    
+    // Add to Realm with transaction
+    [realm beginWriteTransaction];
+    [realm addObject:data];
+    [realm commitWriteTransaction];
 }
 
 @end
